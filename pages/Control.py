dcd79fe2dc8d102dbb858488f845c0b02bad4003
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2025-11-14 09:26:59 -0500

    Update Control.py

--- pages/Control.py
+++ pages/Control.py
@@ -5,7 +5,6 @@ import requests
 import json
 import base64
 import os
-from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
 import warnings
@@ -719,7 +718,6 @@ else:
                 llm,
                 df_cusum,
                 verbose=True,
-                agent_type=AgentType.OPENAI_FUNCTIONS,
                 allow_dangerous_code=True
             )
             
